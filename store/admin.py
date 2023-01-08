aaa20e81945c2b87187c1d577fac82f20de63c07
nomanmajeed <nomanmajeed986@gmail.com>
2023-01-08 20:48:04 +0500

    Prodct model inventory status field added

--- store/admin.py
+++ store/admin.py
@@ -4,8 +4,14 @@ from . import models
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
+    
+    @admin.display(ordering='inventory') # For applying Ordering on Computed Field
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'LOW'
+        return 'OK'
 
 
 admin.site.register(models.Collection)
