36b8c0f5f41ee398ecc70bf77054672ff7b420df
nomanmajeed <nomanmajeed986@gmail.com>
2023-01-08 20:57:01 +0500

    Order model registered

--- store/admin.py
+++ store/admin.py
@@ -4,14 +4,18 @@ from . import models
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_select_related = ['collection'] # qs.select_related for admin
     
     @admin.display(ordering='inventory') # For applying Ordering on Computed Field
     def inventory_status(self, product):
         if product.inventory < 10:
             return 'LOW'
         return 'OK'
+    
+    def collection_title(self, product):
+        return product.collection.title
 
 
 admin.site.register(models.Collection)
@@ -22,3 +26,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 50
+    
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
