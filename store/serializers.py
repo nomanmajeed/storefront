105515b253192b6697d058ad4b8e5ca3ce5c5ea4
nomanmajeed <nomanmajeed986@gmail.com>
2023-01-22 23:15:15 +0500

    Product serialized via ModelSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -29,3 +29,28 @@ class ProductSerializer(serializers.Serializer):
     def calculate_tax(self, product: Product):
         
         return product.unit_price * Decimal(1.2)
+    
+
+class ProductMSerializer(serializers.ModelSerializer):
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection_pkr = serializers.PrimaryKeyRelatedField(
+        queryset=Collection.objects.all(), source='collection'
+    )
+    collection_str = serializers.StringRelatedField(source='collection')
+    collection_hyp = serializers.HyperlinkedRelatedField(
+        view_name='collection-detail',
+        # queryset=Collection.objects.all(),
+        read_only=True,
+        lookup_field='id',
+        source='collection'
+    )
+    collection = CollectionSerializer()
+    
+    def calculate_tax(self, product: Product):
+        
+        return product.unit_price * Decimal(1.2)
+    
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'price', 'price_with_tax', 'collection_pkr', 'collection_str', 'collection_hyp', 'collection']
--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from store.models import *
-from store.serializers import ProductSerializer, CollectionSerializer
+from store.serializers import ProductSerializer, ProductMSerializer, CollectionSerializer
 
 # Create your views here.
 
@@ -17,11 +17,13 @@ def collection_detail(request, id):
 @api_view()
 def products_list(request):
     products = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(products, many=True, context={'request': request})
+    # serializer = ProductSerializer(products, many=True, context={'request': request})
+    serializer = ProductMSerializer(products, many=True, context={'request': request})
     return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
+    # serializer = ProductSerializer(product)
+    serializer = ProductMSerializer(product, context={'request': request})
     return Response(serializer.data)
