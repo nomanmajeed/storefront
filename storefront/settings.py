fc3497341fef461270715a0660954ec1c1fa83ed
nomanmajeed <nomanmajeed986@gmail.com>
2023-01-30 17:53:42 +0500

    Djoser and Simple_JWT installed and configured

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'store',
     'tags',
     'likes',
@@ -132,8 +133,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'core.User'
+
 REST_FRAMEWORK = {
-    'COERCE_DECIMAL_TO_STRING': False
+    'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
-AUTH_USER_MODEL = 'core.User'
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',)
+}
